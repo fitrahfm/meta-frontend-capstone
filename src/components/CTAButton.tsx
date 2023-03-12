89bfe9195260878fcbeb1670bc341c76a5367d27
Fitrah Munir <fitrahfm@Fitrahs-MacBook-Pro.local>
2023-03-12 17:48:01 +0800

    fix cta button size on mobile

--- src/components/CTAButton.tsx
+++ src/components/CTAButton.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const CTAButton: React.FC<{ name: string, target: string }> = ({name, target}) => {
   return (
-    <h4 className='w-fit p-4 bg-[#FBB004] rounded-lg'>
+    <h4 className='w-fit bg-[#FBB004] rounded-lg p-2 lg:p-4 '>
       <a href={ target } type='button'>
         { name }
       </a>
